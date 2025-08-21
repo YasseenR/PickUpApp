e9c21b59fc2f4b789977ce144cafb86d03f14e18
YasseenR <yasseenrouni@gmail.com>
2025-08-21 19:56:09 -0400

    Initial Commit

---
+++ PickUp/PickUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickUpApp.swift
+//  PickUp
+//
+//  Created by Yasseen Rouni on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PickUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
