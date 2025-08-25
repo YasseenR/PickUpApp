0372977f19314bfa903bd297a4654bf58d2d427e
YasseenR <114093367+YasseenR@users.noreply.github.com>
2025-08-25 17:10:12 -0400

    improved event card layout

--- PickUp/Views/EventsPageView.swift
+++ PickUp/Views/EventsPageView.swift
@@ -11,7 +11,8 @@ struct EventsPageView: View {
     
     @State private var events = [
         EventModel(host: "Yasseen Rouni", title: "Pick Up Volleyball", date: "Friday May 30 3:30 PM - 5:30 PM", location: "PH 3rd Floor Gym",sport: "Volleyball", skillLevel: .beginner, maxAttendees: 18, currentAttendees: 10, isOfficial: true, description: "Beginner Friendly Volleyball. Come and learn how to play with others in your same shoes!", imageName: nil),
-        EventModel(host: "Tharani Kannan", title: "All Levels Basketball", date: "Saturday May 30 3:30 PN - 5:30 PM", location: "PH 3rd Floor Gym", sport: "Basketball", skillLevel: .allLevels, maxAttendees: 15, currentAttendees: 13, isOfficial: true, description: "Pick up basketball open to all levels. Come test your skills", imageName: nil)
+        EventModel(host: "Tharani Kannan", title: "All Levels Basketball", date: "Saturday May 30 3:30 PN - 5:30 PM", location: "PH 3rd Floor Gym", sport: "Basketball", skillLevel: .allLevels, maxAttendees: 15, currentAttendees: 13, isOfficial: true, description: "Pick up basketball open to all levels. Come test your skills", imageName: nil),
+        EventModel(host: "Tharani Kannan", title: "All Levels Basketball", date: "Saturday May 30 3:30 PN - 5:30 PM", location: "PH 3rd Floor Gym", sport: "Basketball", skillLevel: .allLevels, maxAttendees: 15, currentAttendees: 15, isOfficial: false, description: "Pick up basketball open to all levels. Come test your skills", imageName: nil)
     ]
     var body: some View {
         VStack {
@@ -51,13 +52,14 @@ struct EventCard: View {
         VStack {
             ZStack(alignment: .topLeading) {
                 Rectangle()
-                    .fill(Color.gray.opacity(0.2))
+                    .fill(Color.black.opacity(0.2))
                     .frame(height: 160)
                     .overlay(
                         Image(systemName: "sportscourt.fill")
                             .font(.system(size: 40))
                             .foregroundColor(.gray)
                     )
+                    .padding(4)
                 
                 HStack(spacing: 4) {
                     Text(eventModel.isOfficial ? "Official" : "Unofficial")
@@ -74,31 +76,40 @@ struct EventCard: View {
                 .padding(12)
             }
             VStack(alignment: .leading, spacing: 4) {
-                
+                Text("\(eventModel.title)")
+                    .font(.headline)
+                //Text("\(eventModel.host)")
+                Text("\(eventModel.sport)")
+                    .font(.caption)
+                Text("\(eventModel.date)")
+                    .font(.subheadline)
+                Text("\(eventModel.location)")
+                    .font(.subheadline)
                 HStack {
                     Text("\(eventModel.host)")
+                        .font(.caption)
                     Spacer()
-                    Text("\(eventModel.sport)")
-                }
-                
-                HStack {
-                    Text("\(eventModel.title)")
-                    Spacer()
-                    Text("\(eventModel.location)")
-                }
-                Text("\(eventModel.date)")
-                HStack {
-                    Text("\(eventModel.skillLevel.rawValue)")
-                    Spacer()
+                    Image(systemName: "person.2.fill")
+                        .foregroundColor(eventModel.isFull ? .warning : .warning)
                     Text("\(eventModel.attendeeStatus)")
+                        .font(.caption)
+                        .foregroundColor(eventModel.isFull ? .warning : .warning)
                 }
+
+                /*Spacer()
+                Text("\(eventModel.location)")
+                Text("\(eventModel.date)")
+                Text("\(eventModel.skillLevel.rawValue)")
+                Text("\(eventModel.attendeeStatus)")*/
             }
+            .padding(4)
         }
         .frame(width: 350, alignment: .topLeading)
         .padding(4)
         .background(
             RoundedRectangle(cornerRadius: 15)
-                .fill(Color(.accentBlue)))
+                .fill(eventModel.isOfficial ? Color(.accentBlue) : Color((.accentBlue)))
+        )
         
     }
 }
