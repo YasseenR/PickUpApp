32277e5407c9bf7fb3715cf06851e00eae511aaf
YasseenR <114093367+YasseenR@users.noreply.github.com>
2025-09-02 11:22:36 -0400

    Update EventsPageView.swift
    Add event details view

--- PickUp/Views/EventsPageView.swift
+++ PickUp/Views/EventsPageView.swift
@@ -15,6 +15,7 @@ struct EventsPageView: View {
         EventModel(host: "Campus Rec", title: "All Levels Basketball", date: "Saturday May 30 3:30 PM - 5:30 PM", location: "PH 3rd Floor Gym", sport: "Basketball", skillLevel: .allLevels, maxAttendees: 15, currentAttendees: 13, isOfficial: true, description: "Pick up basketball open to all levels. Come test your skills", imageName: nil),
         EventModel(host: "Tharani Kannan", title: "All Levels Basketball", date: "Saturday May 30 3:30 PM - 5:30 PM", location: "PH 3rd Floor Gym", sport: "Basketball", skillLevel: .allLevels, maxAttendees: 15, currentAttendees: 15, isOfficial: false, description: "Pick up basketball open to all levels. Come test your skills", imageName: nil)
     ]
+    
     var body: some View {
         VStack {
             VStack(spacing: 20) {
@@ -136,28 +137,75 @@ struct EventCard: View {
     }
 }
 
+struct Players: Identifiable {
+    let id = UUID()
+    let name: String
+}
+
 struct EventDetailsView: View {
     @Binding var eventModel: EventModel
     @Binding var showEvent: Bool
     
+    var players: [Players] = [Players(name: "Alejandro Vasquez"), Players(name:"Chihiro Chizuru"), Players(name:"Naruto Uzumaki"), Players(name:"Yaseen Rouni-belhadj"), Players(name:"Go Dong"), Players(name:"Chief Man"), Players(name: "John Boyer"), Players(name:"Joshua Carte"), Players(name:"Bailey Smith")]
+    
     var body: some View {
         VStack(alignment: .leading) {
-            VStack(alignment: .leading) {
-                Text("Game Details")
-                HStack {
-                    Image(systemName: "calendar")
-                    Text("\(eventModel.date)")
-                    Spacer()
+            VStack(alignment: .leading, spacing: 15) {
+                Rectangle()
+                    .fill(Color.black.opacity(0.2))
+                    .frame(height: 320)
+                    .overlay(
+                        Image(systemName: "sportscourt.fill")
+                            .font(.system(size: 40))
+                            .foregroundColor(.gray)
+                    )
+                    .padding(4)
+                Text(eventModel.title)
+                    //.font(.headline)
+                    .font(.system(size: 40))
+                VStack(spacing: 10) {
+                    HStack {
+                        //Image(systemName: "calendar")
+                        Text("\(eventModel.date)")
+                            .font(.subheadline)
+                        Spacer()
+                    }
+                    HStack {
+                        //Image(systemName: "location")
+                        Text("\(eventModel.location)")
+                            .font(.subheadline)
+                        Spacer()
+                    }
                 }
-                HStack {
-                    Image(systemName: "location")
-                    Text("\(eventModel.location)")
+                Button(action: {}) {
+                    Image(systemName: "calendar.badge.plus")
+                        .padding(10)
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                            .fill(.blue)
+                        )
+                        .foregroundColor(.white)
+                    
                 }
             }
             Spacer()
             
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Players")
+                HStack {
+                    ForEach(players) {player in
+                        VStack {
+                            Circle()
+                                .fill(Color.gray.opacity(0.2))
+                                .frame(width: 30, height: 30)
+                                .overlay(
+                                    Image(systemName: "person.fill")
+                                        .font(.system(size: 15))
+                                        .foregroundColor(.gray)
+                                )
+                        }
+                    }
+                }
             }
             Spacer()
             
@@ -181,5 +229,7 @@ struct EventDetailsView: View {
 }
 
 #Preview {
-    EventsPageView()
+    @State var event = EventModel(host: "Tharani Kannan", title: "All Levels Basketball", date: "Saturday May 30 3:30 PM - 5:30 PM", location: "PH 3rd Floor Gym", sport: "Basketball", skillLevel: .allLevels, maxAttendees: 15, currentAttendees: 15, isOfficial: false, description: "Pick up basketball open to all levels. Come test your skills", imageName: nil)
+    @State var bruh = false
+    EventDetailsView(eventModel: $event, showEvent: $bruh)
 }
